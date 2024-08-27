1210e59b46bc3045ba9527e91d50604a6021c293
lencinajuan <lencina.juan1994@gmail.com>
2024-08-27 09:57:19 -0300

    carrinho de compras completo

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
+let totalGeral;
+limpar();
+
 function adicionar(){
     //Pegar os valores dos campos do formulário: nome do produto, quantidade e valor
     //Calcular o preço, o nosso subtotal (quantidade x valor)
-    //Adicionar ao carrinho
-    //Atualizar valor total da compra
 
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
+    let quantidade = document.getElementById('quantidade').value;
+    //Atualizar valor total da compra
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    //Adicionar ao carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos"><section class="carrinho__produtos__produto">
+        <span class="texto-azul"> ${quantidade} </span>${nomeProduto}<span class="texto-azul">R$${preco}</span></section></section>`
 
-    let preco = quantidade.value * valorUnitario;
-    
-    /* console.log(nomeProduto);
-    console.log(valorUnitario);
-    console.log(quantidade.value);
-    console.log(preco); */
+    //Atualizar valor total da compra
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar(){
-    //Limpar todos os campos após adicionar os itens no carrinho
+    //Função de limpar os dados do carrinho
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').innerHTML = '';
+
 }
